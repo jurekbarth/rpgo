65c8ca5c364c79db74a50d4f922718d2bbbfe902
Jurek Barth <jurek.barth@virtual-identity.com>
2018-12-11 08:55:41 +0100

    update install dir

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ const goLibrary = require('go-library');
 const path = require('path');
 
 const options = {
-  destinationPath: './node_modules/.bin',
+  destinationPath: './bin',
   repo: 'jurekbarth/rpgo',
   version: 'v0.1.5',
   projectname: 'rpgo'
