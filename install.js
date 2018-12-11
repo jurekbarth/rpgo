4477149470a4d47f9a37c7e8723ee50486c82dcc
Jurek Barth <jurek.barth@virtual-identity.com>
2018-12-11 13:09:40 +0100

    npm installable fixes

--- install.js
+++ install.js
@@ -1,8 +1,7 @@
 const goLibrary = require('go-library');
-const path = require('path');
 
 const options = {
-  destinationPath: './bin',
+  destinationPath: 'bin',
   repo: 'jurekbarth/rpgo',
   version: 'v0.1.5',
   projectname: 'rpgo'
