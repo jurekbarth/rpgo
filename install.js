43fb37b70275ce0abde5e432072963120f7a32e3
Jurek Barth <post@jurekbarth.de>
2020-05-20 08:23:10 +0200

    update npm package

--- install.js
+++ install.js
@@ -3,7 +3,7 @@ const goLibrary = require('go-library');
 const options = {
   destinationPath: 'bin',
   repo: 'jurekbarth/rpgo',
-  version: 'v3.0.0',
+  version: 'v3.1.5',
   projectname: 'rpgo'
 }
 
